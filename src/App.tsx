fc84fa011e571ee07e09e71f02d3130d47db0990
unknown <fajarmh2@gmail.com>
2022-03-22 11:20:09 +0700

    Add popup marker in map

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,35 @@ import "./App.css";
 import teslaData from "./data/tesla-sites.json";
 
 function App() {
-
-  const filteredStantions = teslaData.filter(tesla => tesla.address.country === "Italy");
+  const filteredStantions = teslaData.filter(
+    (tesla) => tesla.address.country === "Italy"
+  );
 
   return (
     <div className="App">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+      <MapContainer
+        center={[42.006703, 13.258737]}
+        zoom={6}
+        scrollWheelZoom={true}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
         {filteredStantions.map((tesla) => (
-          <Marker 
-          key= {tesla.id}
-          position={[tesla.gps.latitude, tesla.gps.longitude]}></Marker>
+          <Marker
+            key={tesla.id}
+            position={[tesla.gps.latitude, tesla.gps.longitude]}
+          >
+            <Popup position={[tesla.gps.latitude, tesla.gps.longitude]}>
+              <div>
+                <h2>{"Name: " + tesla.name}</h2>
+                <p>{"Status: " + tesla.status}</p>
+                <p>{"Number of Charging Stations: " + tesla.stallCount}</p>
+              </div>
+            </Popup>
+          </Marker>
         ))}
       </MapContainer>
     </div>
