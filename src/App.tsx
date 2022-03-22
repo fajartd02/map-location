71ab701da416c2b65388282f839aa6aa0d113bc2
unknown <fajarmh2@gmail.com>
2022-03-22 10:52:15 +0700

    create view map from tesla data

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import './App.css';
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "./App.css";
+import teslaData from "./data/tesla-sites.json";
 
 function App() {
   return (
     <div className="App">
       <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {teslaData.map((tesla) => (
+          <Marker 
+          key= {tesla.id}
+          position={[tesla.gps.latitude, tesla.gps.longitude]}></Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
